ab2fc7a05f7946d0f02e452afb310ae46bc65ea5
sasakazu <dropout.college2014@gmail.com>
2017-02-27 14:24:49 +0900

    user/show done.....

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,7 +1,11 @@
 class Space < ApplicationRecord
 
   mount_uploader :image, ImageUploader
+  default_scope -> { order(created_at: :desc) }
 
+  validates :title ,presence: true
+  validates :place ,presence: true
+  validates :image ,presence: true
 
 
 end
